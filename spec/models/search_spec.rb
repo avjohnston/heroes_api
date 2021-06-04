18a9b54e1e4675e3fb85d1015f69a37418384c8a
Andrew Johnston <av.johnst@gmail.com>
2021-06-04 15:11:39 -0600

    Fix failing search model test

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Search, type: :model do
   describe 'instance methods' do 
     it '#search returns supers matching the search' do 
-      search = Search.new(name: 'loki').search
+      search = Search.new(name: 'loki')
     end 
   end 
 end
