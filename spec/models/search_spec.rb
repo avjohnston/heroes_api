fbba09669a61b80d85c1b9283fe897587672f097
Andrew Johnston <av.johnst@gmail.com>
2021-06-07 13:02:42 -0600

    Move search method test from search to super

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe Search, type: :model do
     it { should validate_uniqueness_of(:name).scoped_to(:race, :gender, :full_name, :publisher, :intelligence, :strength, :speed) }
   end
 
-  describe 'instance methods' do 
-    it '#search returns supers matching the search' do 
-      search = Search.new(name: 'loki').search
+  # describe 'instance methods' do 
+  #   it '#search returns supers matching the search' do 
+  #     search = Search.new(name: 'loki').search
 
-      expect(search).to eq(Super.where('name ilike ?', "%loki%"))
-      expect(search[0]).to be_a(Super)
-      expect(search[0].name).to eq('Loki')
-    end 
-  end 
+  #     expect(search).to eq(Super.where('name ilike ?', "%loki%"))
+  #     expect(search[0]).to be_a(Super)
+  #     expect(search[0].name).to eq('Loki')
+  #   end 
+  # end 
 end
